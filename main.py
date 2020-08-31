2cd1a4d0926f86ab42e9505d12a53738802894c3
Marijatrp <marija.trpkovska@student.kyh.se>
2020-08-31 14:50:42 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Jag heter Marija och är uppväxt i Zadar")
+print("Jag har programmerat i programmspråket C på högskolan")
+print("Jag har sökt till KYH pvt eftersom jag skulle vilja vara programvarutestare")
+
